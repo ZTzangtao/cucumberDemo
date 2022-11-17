915b734326fdbcf22ec637b241740b9138fbe4ed
zangtao <TZANG1@ford.com>
2022-11-17 22:13:27 +0800

    init

---
+++ cucumber/src/test/java/com/tommy/cucumber/demo/LemonRunner.java
@@ -0,0 +1,11 @@
+package com.tommy.cucumber.demo;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+/**
+ * @Author Tommy
+ * @Date 2022/11/14 21:39
+ * @Version 1.0
+ */
+public class LemonRunner {
+}
